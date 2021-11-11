7dc849fcb57a4692abe5bf79ab9dadcb674c8779
y23-kimura <y23-kimura@cu.nri.co.jp>
2021-11-11 10:26:10 +0900

    [add] express  & api connection

--- index.js
+++ index.js
@@ -37,6 +37,17 @@ knex(table)
 // migration
 
 // express connection
+const express = require("express");
+const app = express();
+const port = 3000;
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`);
+});
 
 // controller
 
